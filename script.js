16c8e905a83ba434b51c910bd0fd62711e6db40b
AidanVu8 <159743307+AidanVu8@users.noreply.github.com>
2024-03-11 14:14:16 +1100

    Started laying out game

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt("Enter your name");
+let playerGuess;
+let playerFeedback = "test";
+let computerGuess;
+loadDIV ();
 
-if (myIntro == enterName) {
-    alert(myIntro)
+
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 & 10</H2>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber () {
+    playerGuess = prompt("Enter a number")
 }
